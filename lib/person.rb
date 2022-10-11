af9927748a7f23ab2738ac45bc46472f50ec423e
Jennifer Gifford <jgifford82@gmail.com>
2022-10-11 15:53:40 -0600

    commit tree 2043b57f012944781ce82e1b6fb1cf0663728ff2

---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+# Define a Person class, then define an #initialize method that accepts an argument for the person's name. That argument should be stored within a @name instance variable.
+
+class Person
+    def initialize(name)
+        @name = name
+    end
+end
